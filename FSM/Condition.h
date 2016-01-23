73037c5f41ab80614ecb77b0a54a65aa254a5f13
Marco Giordano <marco.giordano.work@gmail.com>
2016-01-23 01:20:36 +0000

    added commenting to condition need to comment connection aswell

--- FSM/Condition.h
+++ FSM/Condition.h
@@ -8,6 +8,9 @@ using FSM::DataType;
 
 namespace FSM
 {
+	/**
+	@brief enum stating the comparison operation to perform
+	*/
 	enum class Operation
 	{
 		EQUAL = 0,
@@ -17,25 +20,50 @@ namespace FSM
 		LESSEQUAL,
 
 	};
-
+	
+	/**
+	@brief, base class for all the condition classes
+	A condition class encapsulate a comparison between two values,
+	this class is used as a poliformic base used inside the state machine.
+	*/
 	class Condition
 	{
 	public:
+		/**
+		@brief constructor
+		@param data: Pointer to a data storage class, the condition
+					 will use it internally to query the value and 
+					 evaluate the condition.
+		*/
 		Condition(DataStorage * data) : m_data(data) {}
+		/**
+		Pure virtual function returning the result of the condition
+		*/
 		virtual bool evaluate() = 0;
 		virtual ~Condition() = default;
 
 	protected:
+		//pointer to the data storage
 		DataStorage * m_data;
 
 	};
 
-
+	/**
+	@brief implements evaluatoin between two different keys	
+	The comparison operation will be triggered between the two provided
+	key, both keys resides in the data storage, aka two variables
+	*/
 	template <typename T>
 	class DoubleVarCondition: public Condition
 	{
 	public:
-
+		/**
+		@brief Constructor
+		@param data: pointer to the data storage	
+		@param key_1_name: the name of the first key of the condition
+		@param key_2_name: the name of the second key of the condition
+		@param op: the operation we want to perform, greather , equal etc
+		*/
 		DoubleVarCondition(DataStorage * data,
 			string& key_1_name,
 			string& key_2_name,
@@ -46,7 +74,12 @@ namespace FSM
 			m_op(op)
 		{ };
 		virtual ~DoubleVarCondition() = default;
-
+		
+		/**
+		@briefThis function evaluates the condition and returns wheter
+		is true or not
+		@returns bool, the result of the condition
+		*/
 		bool evaluate() override
 		{
 			__update_values();
@@ -66,13 +99,21 @@ namespace FSM
 			{return true; }
 			}
 		}
-
+		
+		/**
+		@brief getter function for the first key value
+		@returns T value of the key
+		*/
 		inline T get_value_1()
 		{
 			m_data->get_value(m_key_1_name, m_key_1_value);
 			return m_key_1_value;
 		}
 
+		/**
+		@brief getter function for the second key value
+		@returns T value of the key
+		*/
 		inline T get_value_2()
 		{
 			m_data->get_value(m_key_2_name, m_key_2_value);
@@ -81,50 +122,41 @@ namespace FSM
 
 
 	private:
+		/*
+		Private method for updating both key values
+		*/
 		inline void __update_values()
 		{
 			m_data->get_value(m_key_1_name, m_key_1_value);
 			m_data->get_value(m_key_2_name, m_key_2_value);
 		}
-		//keeping this around just because I am curious to see 
-		//if the compiler is able to optimize the branches out
-		/*
-		template<typename X>
-		inline void __check_key_type(DataType& type)
-		{
-			if (typeid(X) == typeid(bool))
-			{
-				type = DataType::BOOL;
-			}
-			else if (typeid(X) == typeid(int))
-			{
-				type = DataType::INT;
-			}
-			else if (typeid(X) == typeid(float))
-			{
-				type = DataType::FLOAT;
-			}
-			else
-			{
-				type = DataType::BOOL;
-			}
-		}
-		*/
-
 	private:
+		//the two key value storages when evaluated
 		T m_key_1_value;
 		T m_key_2_value;
+		//storage for the two key names
 		string m_key_1_name;
 		string m_key_2_name;
+		//storage for the operation type
 		Operation m_op;
 
 	};
 	
+	/**
+	@brief implements evaluation between a key and a static value 
+	*/
 	template <typename T>
 	class TypedCondition: public Condition
 	{
 	public:
 	
+		/**
+		@brief Constructor
+		@param data: pointer to the data storage	
+		@param key_name: the name of the first key of the condition
+		@param compare_to_value: the value we want to compair against
+		@param op: the operation we want to perform, greather , equal etc
+		*/
 		TypedCondition(DataStorage * data,
 			string& key_name,
 			T compare_to_value,
@@ -134,14 +166,26 @@ namespace FSM
 			m_compare_to_value(compare_to_value),
 			m_op(op)
 		{ };
-
+		/**
+		Destructor
+		*/
 		virtual ~TypedCondition() = default;
 
+		/**
+		Getter function for the key value
+		@returns: the T value for the key 
+		*/
 		inline T get_value()
 		{
 			m_data->get_value(m_key_name, m_key_value);
 			return m_key_value;
 		}
+
+		/**
+		@briefThis function evaluates the condition and returns wheter
+		is true or not
+		@returns bool, the result of the condition
+		*/
 		bool evaluate() override
 		{
 			m_data->get_value(m_key_name, m_key_value);
@@ -162,9 +206,37 @@ namespace FSM
 			}
 		}
 	private:
+		//storage for the key value
 		T m_key_value;
+		//storage for the key nane
 		string m_key_name;
+		//storage for the comparison value
 		T m_compare_to_value;
+		//storage for the operation
 		Operation m_op;
 	};
 }
+//keeping this around just because I am curious to see 
+//if the compiler is able to optimize the branches out
+/*
+template<typename X>
+inline void __check_key_type(DataType& type)
+{
+	if (typeid(X) == typeid(bool))
+	{
+		type = DataType::BOOL;
+	}
+	else if (typeid(X) == typeid(int))
+	{
+		type = DataType::INT;
+	}
+	else if (typeid(X) == typeid(float))
+	{
+		type = DataType::FLOAT;
+	}
+	else
+	{
+		type = DataType::BOOL;
+	}
+}
+*/
