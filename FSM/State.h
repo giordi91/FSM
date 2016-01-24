8789555ed026f320f8d838a3b015a7836a9e13ff
Marco Giordano <marco.giordano.work@gmail.com>
2016-01-24 12:46:53 +0000

    added comment on State.h

--- FSM/State.h
+++ FSM/State.h
@@ -10,21 +10,33 @@ namespace FSM
 	class State
 	{
 	public:
+		/**
+		@bief constructor
+		@param name: string represeting the name
+		*/
 		State(string& name) :m_name(name) {}
+		/**
+		@bief constructor
+		@param name: string represeting the name
+		*/
 		State(string name) :m_name(name) {}
-		
+
+		/**
+		@brief adds a connection to the state
+		@param conn: pointer to the connection we wish to add
+		*/
 		inline void add_connection(Connection * conn)
 		{
 			m_connections.push_back(conn);
 		};
 		~State() = default;
-		
+
 		/**
 		@brief evaluates wheter there is need for a transition
-		This function is going to evaluate all the connecitions and 
+		This function is going to evaluate all the connecitions and
 		if one evaluate to true will return that a pointer to the next
 		state. The order in which connections are added gives precedence.
-		@returns : returns a pointer to the next state, if none of the 
+		@returns : returns a pointer to the next state, if none of the
 		connections evaluate to true will return a pointer to itself
 		*/
 		inline State * transition()
@@ -38,10 +50,21 @@ namespace FSM
 			}
 
 			return this;
-		
+
 		};
+		/**
+		@brief Getter method for the state name
+		@returns a const ref to the name
+		*/
+		inline const string& get_name()const
+		{
+			return m_name;
+		}
+
 	private:
+		//storagfe for the connection pointers
 		vector<Connection*> m_connections;
+		//the state name
 		string m_name;
 	};
 
