0aabecf347b3dd9fb34617499d9125cda4e86e13
Marco Giordano <marco.giordano.work@gmail.com>
2016-03-03 00:19:06 +0000

    added basic template selector for typed condition, i can now move in adding the Double condition

---
+++ FSMEditor/ConditionTemplateSelector.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using FSMWrapper;
+
+namespace FSMEditor
+{
+    class ConditionTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate TypedConditionTemplate { get; set; }
+        public DataTemplate DoubleConditionTemplate { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            var cond = item as ConditionBinding;
+            if ( cond.m_wrap_cond is ITypedCondition<int> ||
+                cond.m_wrap_cond is ITypedCondition<bool> ||
+                cond.m_wrap_cond is ITypedCondition<float> )
+            {
+                return TypedConditionTemplate;
+            }
+             return base.SelectTemplate(item, container);
+        }
+
+    }
+}
